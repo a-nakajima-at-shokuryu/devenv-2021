3b021f32593765664a89c9b96073ec85b29e4e12
unknown <a-nakajima@shokuryu.co.jp>
2021-05-09 17:31:32 +0900

    Python3.9.5 & node-v16.1.0

---
+++ .vendor/activate.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%~dp0node-v16.1.0-win-x64;%PATH%
+set PATH=%~dp0Python39;%PATH%
+set PATH=%~dp0Python39\Scripts;%PATH%
---
+++ readme.md
@@ -0,0 +1,9 @@
+# devenv-2021
+
+```bash 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+```
+
+```bash
+python get-pip.py
+```
